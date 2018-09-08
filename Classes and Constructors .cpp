d64d7469d06d441512eb5398b5c396eedc339608
Mohamed Barakat <MohamedBarakatSE@gmail.com>
2018-09-08 13:35:56 +0200

    First Initilization

---
+++ Classes and Constructors .cpp
@@ -0,0 +1,51 @@
+#include <iostream>    // using IO functions
+#include <string>      // using string
+#include<math.h>
+using namespace std;
+#define _USE_MATH_DEFINES
+ class Circle
+ {
+ private:
+     /* data members */
+    double radius; 
+    string color;
+ public:
+ /*Circle(double r , string c): radius(r),color(c){ } another constructor defenition */
+ Circle(double r=1.5,string c="Red") {
+ radius=r;
+ color=c;
+ }
+
+    string getColor() {
+
+        return color;
+    }
+     double getRadius(/* args */){
+         return radius;
+     }
+     double getArea() {
+
+         //return radius*radius*3.1416; 
+         return radius*radius*M_PI; // Including PI using math.h
+     }
+
+ };
+ 
+
+int main() {
+ // Construct a Circle instance
+   Circle c1(1.2, "blue");
+   cout << "Radius=" << c1.getRadius() << " Area=" << c1.getArea()
+        << " Color=" << c1.getColor() << endl;
+ 
+   // Construct another Circle instance
+   Circle c2(3.4); // default color
+   cout << "Radius=" << c2.getRadius() << " Area=" << c2.getArea()
+        << " Color=" << c2.getColor() << endl;
+ 
+   // Construct a Circle instance using default no-arg constructor
+   Circle c3;      // default radius and color
+   cout << "Radius=" << c3.getRadius() << " Area=" << c3.getArea()
+        << " Color=" << c3.getColor() << endl;
+   return 0;
+}
